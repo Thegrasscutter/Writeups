94ef1dab8f59483e2f710e2492efffdff3c5c3f2
jezpoz <jesfor94@gmail.com>
2022-03-03 15:51:35 +0100

    add tailwind and alpine.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,31 @@
+const now = String(Date.now());
+const htmlmin = require('html-minifier');
+
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addWatchTarget('./styles/tailwind.config.js');
+	eleventyConfig.addWatchTarget('./styles/tailwind.css');
+	eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' });
+	eleventyConfig.addShortcode('version', function () {
+		return now
+	});
+	eleventyConfig.addTransform('htmlmin', function (content, outpath) {
+		if (process.env.ELEVENTY_PRODUCTION && outpath && outpath.endsWith('.html')) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true,
+			});
+			return minified;
+		}
+		return content;
+	});
+	eleventyConfig.addPassthroughCopy({
+		'./node_modules/alpinejs/dist/cdn.js': './js/alpine.js',
+	})
 	return {
 		dir: {
 			input: "site",
-      output: "dist"
+			output: "dist"
 		}
 	}
 }
